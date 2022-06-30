4196700aeaebb781eb82cfa20250aa4e34a34391
sec-halit <102877470+sec-halit@users.noreply.github.com>
2022-07-01 00:23:03 +0300

    dev-04 vercel deploy

--- api/userRoutes.js
+++ api/userRoutes.js
@@ -9,6 +9,6 @@ const { protect } = require('../middleware/authMiddleware')
 
 //router.post('/register', registerUser)
 router.post('/login', loginUser)
-//router.get('/me', protect, getMe)
+router.get('/me', protect, getMe)
 
 module.exports = router
